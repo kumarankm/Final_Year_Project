09865b80855e1573443a8fb6144c76a5023c844c
Kumaran KM <kumaranmohan1106@gmail.com>
2022-05-12 16:17:08 +0530

    Add files via upload

---
+++ Code_Progress/EncryptionPart.py
@@ -0,0 +1,5 @@
+def encrypt(msg):
+    cipher = AES.new(key, AES.MODE_EAX)
+    nonce = cipher.nonce
+    ciphertext, tag = cipher.encrypt_and_digest(msg.encode('ascii'))
+    return nonce, ciphertext, tag
